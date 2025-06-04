bb05d8e4cafadd8fef5f904fe76072c14b38e7d1
Mauricio <aresmauricio33@gmail.com>
2025-06-04 17:24:44 +0200

    Exam Simulator

--- tests/test_level1/ulstr.sh
+++ tests/test_level1/ulstr.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-EXE=../../current_exercise/level1/ulstr
-
-gcc "$EXE.c" -o ulstr_test_exec || { echo "❌ Compilation failed"; exit 1; }
-
 run_test() {
     expected="$1"
     shift
-    output=$(./ulstr_test_exec "$@" | cat -e)
+    output=$("$1" "${@:2}" | cat -e)
 
     if [ "$output" == "$expected" ]; then
-        echo "✅ Passed: $*"
+        echo "✅ Passed: ${*:2}"
         return 0
     else
-        echo "❌ Failed: $*"
+        echo "❌ Failed: ${*:2}"
         echo "Expected: $expected"
         echo "Got:      $output"
         return 1
@@ -22,11 +18,9 @@ run_test() {
 
 fail=0
 
-run_test "l'EspRIT Ne PEuT PLuS PrOGrESsER S'iL STAgNE ET Si PErSiStENT vaNiTE ET AUTO-JUSTIFICATION.$" "L'eSPrit nE peUt plUs pRogResSer s'Il staGne et sI peRsIsTent VAnIte et auto-justification."
-run_test "s'ENtoUrER De SecREt EsT Un SigNe dE MaNqUe dE COnnAIssANcE.  $" "S'enTOuRer dE sECreT eSt uN sIGnE De mAnQuE De coNNaiSSanCe.  "
-run_test "3:21 bA  ToUT  MOuN KI kA DI ke M'EN kA FE FOT$" "3:21 Ba  tOut  moUn ki Ka di KE m'en Ka fe fot"
-run_test "$"
-
-rm -f ulstr_test_exec
+run_test "l'EspRIT Ne PEuT PLuS PrOGrESsER S'iL STAgNE ET Si PErSiStENT vaNiTE ET AUTO-JUSTIFICATION.$" "$1" "L'eSPrit nE peUt plUs pRogResSer s'Il staGne et sI peRsIsTent VAnIte et auto-justification."
+run_test "s'ENtoUrER De SecREt EsT Un SigNe dE MaNqUe dE COnnAIssANcE.  $" "$1" "S'enTOuRer dE sECreT eSt uN sIGnE De mAnQuE De coNNaiSSanCe.  "
+run_test "3:21 bA  ToUT  MOuN KI kA DI ke M'EN kA FE FOT$" "$1" "3:21 Ba  tOut  moUn ki Ka di KE m'en Ka fe fot"
+run_test "$" "$1"
 
 exit $fail
